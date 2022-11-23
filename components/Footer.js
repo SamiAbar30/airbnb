aedfe44d0750503db8e3e15f263edfcc3860e0c7
SamiAbar30 <samiabar30@gmail.com>
2022-11-23 10:46:55 +0100

    addd footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,38 +2,38 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='grid gird-cols-1 md:gird-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>ABOUT</h5>
-        <p className=''>How Airbnb works</p>
-        <p className=''>Newsroom</p>
-        <p className=''>Investors</p>
-        <p className=''>Airbnb Plus</p>
-        <p className=''>Airbnb Luxe</p>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
       </div>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>COMMUNITY</h5>
-        <p className=''>Accessibility</p>
-        <p className=''>This is not a real site</p>
-        <p className=''>Its a pretty awsome clone</p>
-        <p className=''>Referrals accepted</p>
-        <p className=''>Papafam</p>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awsome clone</p>
+        <p>Referrals accepted</p>
+        <p>Papafam</p>
       </div>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>HOST</h5>
-        <p className=''>Papa React</p>
-        <p className=''>Presents</p>
-        <p className=''>Zero to Full Stack Hero</p>
-        <p className=''>Hundreds of students</p>
-        <p className=''>Join Now</p>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Zero to Full Stack Hero</p>
+        <p>Hundreds of students</p>
+        <p>Join Now</p>
       </div>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>SUPPORT</h5>
-        <p className=''>Help Centre</p>
-        <p className=''>Trust & Safety</p>
-        <p className=''>Say Hi YouTube</p>
-        <p className=''>Easter Eggs</p>
-        <p className=''>For the Win</p>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
       </div>
       
     </div>
