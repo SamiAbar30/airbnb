c588481c2c8fb3f859b4e2ac262bc76a02cf1103
SamiAbar30 <samiabar30@gmail.com>
2022-11-25 17:02:09 +0100

    change map version

--- components/MapAside.js
+++ components/MapAside.js
@@ -1,33 +1,55 @@
 import { getCenter } from 'geolib';
-import Map, { Marker, Popup } from 'react-map-gl';
+import { useState } from 'react';
+import ReactMapGL , { Marker, Popup } from 'react-map-gl';
 
 function MapAside({ searchResults }) {
+
+ const [selectedLocation, setSelectedLocation] = useState()
 	const mapResults = searchResults.map((item) => ({
 		latitude: item.lat,
 		longitude: item.long,
 	}));
 	const center = getCenter(mapResults);
+	const [viewState, setViewState] = useState({
+		width:"100%",
+		height:"100%",
+		longitude: center.longitude,
+		latitude: center.latitude,
+		zoom: 11,
+	});
 	return (
-		<Map
-			initialViewState={{
-				longitude: center.longitude,
-				latitude: center.latitude,
-				zoom: 8,
-			}}
+		<ReactMapGL 
+			{...viewState}
 			mapStyle='mapbox://styles/sami-abar-mapbox1/clavnhfaf00ce14ohql828pf2'
-			mapboxAccessToken={process.env.mapbox_key}>
+			onViewportChange={evt => setViewState(evt)}
+			mapboxApiAccessToken={process.env.mapbox_key}>
 			{searchResults.map((result) => (
 				<div key={result.long}>
 					<Marker
+					role="img"
 						longitude={result.long}
 						latitude={result.lat}
+						offsetTop={-10}
 						offsetLeft={-20}
-						offsetTop={-10}>
-						<p className='cursor-ponter text-2xl animate-bounce'>📌</p>
+						
+						>
+						<p onClick={()=>setSelectedLocation(result)} className='cursor-pointer text-2xl animate-bounce'
+						aria-label='push-pin'
+						>📌</p>
 					</Marker>
+					{selectedLocation?.long === result.long&&(
+						<Popup 
+						onClose={()=>setSelectedLocation({})} 
+						closeOnClick={true} 
+						longitude={result.long}
+						latitude={result.lat}
+						>
+							{result.title}
+						</Popup>
+					)}
 				</div>
 			))}
-		</Map>
+		</ReactMapGL>
 	);
 }
 
